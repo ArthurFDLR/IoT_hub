d2674d217eaa7cff2e792d7b29a3a52b1f5d777e
ArthurFDLR <arthfind@gmail.com>
2021-05-09 14:50:41 -0500

    🧹Remove config files

--- clear_mqtt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#recquiered package: mosquitto-clients 
-echo "cleaning retained MQTT messages on localhost"
-timeout 1s mosquitto_sub -h localhost -t "#" -v -R | while read line; do mosquitto_pub -h localhost -t "${line% *}" -r -n; done
